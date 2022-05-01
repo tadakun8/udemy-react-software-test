fe67c98e16efdd405a839410ac3765127eecdf24
tadakun8 <tadakun8.lab@gmail.com>
2022-05-01 16:14:42 +0900

    [Redux] cerateSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import customCounterSlice from "../features/customCounter/customeCounterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    custonCounter: customCounterSlice,
   },
 });
---
+++ src/features/customCounter/customeCounterSlice.js
@@ -0,0 +1,80 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const sleep = (msec) => {
+  const start = new Date();
+  while (new Date() - start < msec);
+};
+
+export const fetchDummy = createAsyncThunk("featch/dummy", async (num) => {
+  await sleep(2000);
+  return num;
+});
+
+export const fetchJSON = createAsyncThunk("fetch/api", async () => {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/users/1");
+  return response.data.username;
+});
+
+export const customCounterSlice = createSlice({
+  name: "customCounter",
+  initialState: {
+    mode: 0,
+    value: 0,
+    username: "",
+  },
+  reducers: {
+    increment: (state) => {
+      switch (state.mode) {
+        case 0:
+          state.value += 1;
+          break;
+        case 1:
+          state.value += 100;
+          break;
+        case 2:
+          state.value += 10000;
+          break;
+        default:
+          break;
+      }
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      switch (state.mode) {
+        case 0:
+          state.value += action.payload;
+          break;
+        case 1:
+          state.value += 100 * action.payload;
+          break;
+        case 2:
+          state.value += 10000 * action.payload;
+          break;
+        default:
+          break;
+      }
+    },
+  },
+  // extraReducers: 非同期関数を扱う場合に使う
+  extraReducers: (builder) => {
+    builder.addCase(fetchDummy.fulfilled, (state, action) => {
+      state.value = 100 + action.payload;
+    });
+    builder.addCase(fetchDummy.rejected, (state, action) => {
+      state.value = 100 - action.payload;
+    });
+    builder.addCase(fetchJSON.fulfilled, (state, action) => {
+      state.username = action.payload;
+    });
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = customCounterSlice.actions;
+
+export const selectCount = (state) => state.customeCounter.value;
+export const selectUsername = (state) => state.customeCounter.username;
+
+export default customCounterSlice.reducer;
